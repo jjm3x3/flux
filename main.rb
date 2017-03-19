34b4191aeeaed52643d97e8bc0080ee9efba54e9
Jacob M <jjm3333@gmail.com>
2017-03-19 13:15:11 -0700

    Start to add a gui for the game

--- main.rb
+++ main.rb
@@ -1,16 +1,43 @@
 require "./game.rb"
 require "gosu"
 
+class Card
+  def initializ()
+
+  end
+
+  def draw(x,y,z=0)
+    Gosu.draw_rect(10,10,40,80,Gosu::Color.argb(0xff_ff0000))
+  end
+end
+
 class GameWindow < Gosu::Window
-  def initialize(width=320, height=240, fullscreen=false)
+  def initialize(width=320, height=240, fullscreen=true)
     super
     self.caption = 'Fluxx'
     @message = Gosu::Image.from_text(
-      self, 'something', Gosu.default_font_name, 30)
+      self, 'somethingAnd something else', Gosu.default_font_name, 8)
+    @rect = Card.new
   end
 
   def draw
+    # @rect.draw(10,10)
     @message.draw(10,10,0)
+  end
+
+  def button_down(id)
+    if id == Gosu::MsLeft
+      puts mouse_x
+      puts mouse_y
+    end
+  end
+
+  def find_card(x,y)
+
+  end
+
+  def needs_cursor?
+    true
   end
 
 end
@@ -29,7 +56,6 @@ if HEADLESS
   game = Game.new
   game.run
 else
-  puts "huh?!"
   window = GameWindow.new
   window.show
 end
