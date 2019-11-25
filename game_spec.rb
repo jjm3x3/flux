88530fa74db7cfbe1af5264973d1efa8dd8994b3
Jacob Meixner <jjm3333@gmail.com>
2019-11-24 17:22:50 -0800

    Add first test about rotateHands

--- game_spec.rb
+++ game_spec.rb
@@ -2,10 +2,28 @@ require "./game.rb"
 require "Tempfile"
 
 describe "game" do
+
+    test_oufile = Tempfile.new 'test_output'
+
     it "should construct" do
-        test_oufile = Tempfile.new 'test_output'
         Game.new("input_stream", test_oufile)
-        test_oufile.unlink
     end 
 
+    describe "rotate_hands" do
+        it "first player should not have the hand they started with" do
+            # setup
+            input_stream = StringIO.new("thing")
+            theGame = Game.new(input_stream, test_oufile)
+            theFirstPlayer = theGame.players[0]
+            firstPlayersOriginalCards = theFirstPlayer.hand
+
+            # execute
+            theGame.rotateHands(theFirstPlayer)
+
+            # test
+            expect(firstPlayersOriginalCards).not_to eq theFirstPlayer.hand
+        end
+    end
+
+    test_oufile.unlink
 end
