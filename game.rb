9c374033f516c6d614ef737d8c97893c2415415c
Jacob Meixner <jjm3333@gmail.com>
2019-11-30 18:56:08 -0800

    Fix a problem with printKeepers in the game

--- game.rb
+++ game.rb
@@ -49,7 +49,7 @@ class Game
     @interface.information "the discard has #{@discardPile.length} card(s) in it"
     @interface.information "here is the current goal: #{@goal }"
     @interface.information "here are the current rules:\n#{@ruleBase}"
-    @interface.printKeepers(activePlayer.keepers, "Here are your keepers")
+    @interface.printKeepers(activePlayer)
     cardsPlayed = 0
     cardsDrawn = @ruleBase.drawRule
     hand = activePlayer.hand
