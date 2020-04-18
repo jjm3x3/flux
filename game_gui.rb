da0c4e9f2d122cbfe54a1dd8943bffddc250888e
Jacob Meixner <jjm3333@gmail.com>
2020-04-18 16:12:56 -0700

    Get button clicking mechanism down

--- game_gui.rb
+++ game_gui.rb
@@ -7,10 +7,24 @@ class GameGui < Gosu::Window
 
         @bakground_image = Gosu::Image.new("assets/onlinePurpleSquare.jpg", tileable: true)
         @font = Gosu::Font.new(20)
+
+        @left_click_down = false
+    end
+
+    def button_up(id)
+        if @left_click_down
+            puts "left button released"
+            @left_click_down = false
+        end
     end
 
     def update
 
+        if Gosu.button_down? Gosu::MS_LEFT and !@left_click_down
+            puts "left button click"
+            @left_click_down = true
+
+        end
     end
 
     def draw
