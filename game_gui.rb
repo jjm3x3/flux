99a748cb3cf052391ade1e1bfb0c62efaa12d919
Jacob Meixner <jjm3333@gmail.com>
2020-04-18 15:58:00 -0700

    Add some elements to the game

--- game_gui.rb
+++ game_gui.rb
@@ -4,6 +4,9 @@ class GameGui < Gosu::Window
     def initialize
         super 640, 480
         self.caption = "Fluxx"
+
+        @bakground_image = Gosu::Image.new("assets/onlinePurpleSquare.jpg", tileable: true)
+        @font = Gosu::Font.new(20)
     end
 
     def update
@@ -11,6 +14,8 @@ class GameGui < Gosu::Window
     end
 
     def draw
+        @bakground_image.draw(0,0,0)
+        @font.draw_text("Here is some text", 10,10, 1 , 1.0, 1.0, Gosu::Color::WHITE)
     end
 end
 
