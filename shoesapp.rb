de9c03add4d90e2ae1c534b8bdaf0e4d8b0daedb
Jacob Meixner <jjm3333@gmail.com>
2020-04-18 14:11:53 -0700

    Display draw pile and discard pile counts

--- shoesapp.rb
+++ shoesapp.rb
@@ -9,8 +9,12 @@ Shoes.app do
 
     end
 
+    game = Game.new(3, GuiInterface.new(self))
+
     @game_stack = stack do
         para "Is this all new :thinking:"
+        para "The deck has #{game.deck.count} cards remaining"
+        para "The discard has #{game.discardPile.size} cards in it"
     end
 
     @game_stack.hide
@@ -18,7 +22,6 @@ Shoes.app do
     @startGameButton.click do
         @game_stack.show
         @startStack.hide
-        game = Game.new(3, GuiInterface.new(self))
         # game.run
     end
 end
