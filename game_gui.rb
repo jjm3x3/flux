a16f33ff7ecd86f044c676fbf4500ea575ef3772
Jacob Meixner <jjm3333@gmail.com>
2020-04-25 17:46:33 -0700

    Remove custom cursor entirely

--- game_gui.rb
+++ game_gui.rb
@@ -9,7 +9,6 @@ class GameGui < Gosu::Window
         self.caption = "Fluxx"
 
         @bakground_image = Gosu::Image.new("assets/onlinePurpleSquare.jpg", tileable: true)
-        @cursor = Gosu::Image.new("assets/onlineCursor2.png")
         @font = Gosu::Font.new(20)
 
         @left_click_down = false
@@ -70,7 +69,6 @@ class GameGui < Gosu::Window
 
     def draw
         @bakground_image.draw(0,0,0)
-        #@cursor.draw(mouse_x, mouse_y, 2, 0.0078125, 0.0078125)
 
         if !@game_driver
         # for main menu
