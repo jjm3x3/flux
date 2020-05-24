0fbbab86325071bdafe23931bf7d8d75a2fdf9e5
Jacob Meixner <jjm3333@gmail.com>
2020-05-23 20:08:51 -0700

    Restore functionality to select_a_card

--- tests/gui_input_manager_spec.rb
+++ tests/gui_input_manager_spec.rb
@@ -1,28 +1,16 @@
 require "./cards/cards.rb"
 require "./gui_input_manager.rb"
 
-require "pry-byebug"
-
 describe "gui_input_manager" do
 
     describe "select_a_card" do
         it "works?" do
-            # debugger
-            guiDouble = double("gui")
+            testCard = Card.new
+            guiDouble = double("gui", :select_a_card => nil, :get_dialog_result => testCard)
             sut = GuiInputManager.new(guiDouble)
 
-            aSingleCard = Card.new
-            puts "What is this: '#{aSingleCard}'"
-            select_result = sut.await.select_a_card([aSingleCard], "prompt for a test")
-            # select_result = sut.select_a_card([Card.new], "prompt for a test")
+            select_result = sut.await.select_a_card([testCard], "prompt for a test")
 
-            # binding.pry
-            puts "Here is the raw result: '#{select_result}''"
-            puts "Here is the state  '#{select_result.state}'"
-            if select_result.state == :rejected
-                puts "For reason '#{select_result.reason}'"
-            end
-            puts "Here is the result '#{select_result.value}'"
             expect(select_result).not_to be nil
         end
     end
