f4f0b1a2ed91d77344791d9d677d8b39dd8f5887
Jacob Meixner <jjm3333@gmail.com>
2020-05-25 14:26:36 -0700

    Start conversation to new_game_driver for game_cli

--- game_cli.rb
+++ game_cli.rb
@@ -8,25 +8,24 @@ class GameCli
 
   def run
       loop do
-        activePlayer = @game_driver.active_player
+        activePlayer = @new_game_driver.await.active_player.value
         @interface.display_game_state(@game_driver.game)
         @logger.information "\n#{activePlayer}'s turn"
 
-        @game_driver.setup_new_turn
+        @new_game_driver.await.setup_new_turn
         hand = activePlayer.hand
         cardsPlayed = 0
-        while !@game_driver.turn_over?
+        while !@new_game_driver.await.turn_over?.value
           @logger.printPermanents(activePlayer)
 
           cardToPlay = @logger.select_a_card(hand, "Select a card from your hand to play")
 
-          @game_driver.post_card_play_clean_up(activePlayer, cardToPlay)
+          @new_game_driver.await.post_card_play_clean_up(activePlayer, cardToPlay)
           cardsPlayed += 1
 
           hand = activePlayer.hand # really a sad sideeffect of much statefull programming
           @logger.information "played: #{cardsPlayed} of play: #{@game_driver.game.ruleBase.playRule}"
         end
-        @game_driver.end_turn_cleanup
       end
   end
 
