922038caaa3323b017c89b4b50c7cddd573fe3fc
Jacob Meixner <jjm3333@gmail.com>
2020-05-25 18:47:13 -0700

    Stop using game as an async

--- game_driver.rb
+++ game_driver.rb
@@ -16,9 +16,9 @@ class GameDriver
 
     def setup_new_turn
       if(active_player.has_death?)
-        @game.await.resolve_death_rule(active_player)
+        @game.resolve_death_rule(active_player)
       end
-      drawnCards = @game.await.drawCards(active_player, :draw_rule).value
+      drawnCards = @game.drawCards(active_player, :draw_rule)
       active_player.add_cards_to_hand(drawnCards)
       @cardsPlayed = 0
       @cardsDrawn = drawnCards.length
@@ -28,24 +28,23 @@ class GameDriver
         @logger.debug "Beginning of turn_over?"
         active_player_has_cards = active_player.hand.length > 0
         @logger.debug "Does the active player have cards #{active_player_has_cards}"
-        result = @cardsPlayed >= @game.await.play_limit.value || !active_player_has_cards
+        result = @cardsPlayed >= @game.play_limit || !active_player_has_cards
         @logger.debug "Is the turn over? #{result}"
         return result
     end
 
     def post_card_play_clean_up(player, card_to_play)
         @logger.debug "this should get logged sync"
-        @game.await.play_card(card_to_play, player)
-        # card_to_play.await.play(player, @game)
+        @game.play_card(card_to_play, player)
         @logger.debug "After card was played"
         @cardsPlayed += 1
         @logger.debug "Increment cards played"
         checkForWinner # should check for a winner before discarding
         @logger.debug "Checked for winner"
-        @game.await.enforceNonActivePlayerLimits(player)
+        @game.enforceNonActivePlayerLimits(player)
         @logger.information "the discard has #{@game.discardPile.length} card(s) in it"
         # do something if the discard need reshufleing
-        @cardsDrawn = @game.await.replenishHand(@cardsDrawn, player).value
+        @cardsDrawn = @game.replenishHand(@cardsDrawn, player)
         @logger.debug "Finished post_card_play_clean_up"
         if turn_over?
             @logger.debug "The turn is over proceed to end_turn_cleanup"
@@ -56,22 +55,22 @@ class GameDriver
     end
 
     def end_turn_cleanup
-      @game.await.discardDownToLimit(active_player)
-      @game.await.removeDownToKeeperLimit(active_player)
+      @game.discardDownToLimit(active_player)
+      @game.removeDownToKeeperLimit(active_player)
       if(@take_another_turn)
         @currentPlayerCounter -= 1
         @take_another_turn = false
       end
-      @game.await.progress_turn
+      @game.progress_turn
     end
 
     def active_player
-        @game.await.active_player.value
+        @game.active_player
     end
 
     private
     def checkForWinner
-      if @game.await.winner.value
+      if @game.winner
         puts "the game is over!!!!==============\\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/"
         exit 0
       end
