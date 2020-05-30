e39e484749b2bfbf4e67b1fb233268a2a635226e
Jacob Meixner <jjm3333@gmail.com>
2020-05-30 12:00:15 -0700

    Add some log lines to help with debuggin

--- game.rb
+++ game.rb
@@ -229,17 +229,21 @@ class Game
   end
 
   def todaysSpecial(player)
+    @logger.debug "Executing todays_special"
     drawnCards = drawCards(player, 3)
     cardToPlay = @interface.await.choose_from_list(drawnCards, "pick a card to play").value
     cardToPlay.play(player, self)
 
+    @logger.debug "First card played now figure out if any more should be played"
     if @interface.await.ask_yes_no("is today your birthday").value
+      @logger.debug "It is the current players birthday"
       cardToPlay = @interface.await.choose_from_list(drawnCards, "pick a card to play").value
       cardToPlay.play(player, self)
 
       cardToPlay = @interface.await.choose_from_list(drawnCards, "pick a card to play").value
       cardToPlay.play(player, self)
     else
+      @logger.debug "It is the not current players birthday is it at least a holiday or anniversary"
       if @interface.await.ask_yes_no("Is today a holiday or an anniversary").value
         cardToPlay = @interface.await.choose_from_list(drawnCards, "pick a card to play").value
         cardToPlay.play(player, self)
