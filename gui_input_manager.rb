39f7c16fd0dc7270215b05e3a4253bd24fe9c37f
Jacob Meixner <jjm3333@gmail.com>
2020-05-30 15:45:57 -0700

    Remove extraneous logging from gui_input_manager

--- gui_input_manager.rb
+++ gui_input_manager.rb
@@ -10,31 +10,13 @@ class GuiInputManager
     end
 
     def choose_from_list(list, prompt)
-        # @input_manager_log = File.open("input_manager.log", "a")
-        puts "goint to display a card selection dialog"
-        # @input_manager_log.puts "goint to display a card selection dialog"
         @gui.display_list_dialog(list, prompt)
-        puts "after dialog displayed"
-        # @input_manager_log.puts "after dialog displayed"
-        # @input_manager_log.flush
+
         dialog_result = nil
-        puts "going into wait loop"
-        # @input_manager_log.puts "going into wait loop"
-        # @input_manager_log.puts "what is the dialog results '#{dialog_result}'"
-        # @input_manager_log.puts "what is the condition used for the loop '#{!dialog_result}'"
-        # @input_manager_log.flush
         while !dialog_result
-            # @input_manager_log.puts "Going to sleep since nothing is selected at #{Time.now}"
-            # @input_manager_log.flush
             sleep @sleep_amount
-            # @input_manager_log.puts "checking dialog_result at #{Time.now}"
-            # @input_manager_log.flush
             dialog_result = @gui.get_dialog_result
-            # @input_manager_log.puts "What is the value of #{dialog_result}"
-            # @input_manager_log.flush
         end
-        # @input_manager_log.puts "What is the dialog_result: '#{dialog_result}''"
-        # @input_manager_log.flush
         list.delete(dialog_result)
         return dialog_result
     end
