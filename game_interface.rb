6a10d3fbbf49366640ffc8eed67053a711e3f95a
Jacob Meixner <jjm3333@gmail.com>
2020-05-30 18:25:52 -0700

    Implement BaseTextInterface sbstraction to reducde duplicate code

--- game_interface.rb
+++ game_interface.rb
@@ -18,21 +18,11 @@ class StringFormattingUtilities
   end
 end
 
-class CliInterface
+class BaseTextInterface
   include Concurrent::Async
 
   def initialize
     super()
-    @output_stream = $stdout
-    @input_stream = $stdin
-  end
-
-  def display_game_state(game)
-    @output_stream.puts "\e[2J\e[f"
-    @output_stream.puts "The deck has #{game.deck.count} cards in it"
-    @output_stream.puts "the discard has #{game.discardPile.length} card(s) in it"
-    @output_stream.puts "here is the current goal: #{game.goal}"
-    @output_stream.puts "here are the current rules:#{game.ruleBase}"
   end
 
   def choose_from_list(card_list, prompt="select a card")
@@ -61,13 +51,30 @@ class CliInterface
   end
 end
 
-class TestInterface
-  include Concurrent::Async
+class CliInterface < BaseTextInterface
+
+  def initialize
+    super()
+    @output_stream = $stdout
+    @input_stream = $stdin
+  end
+
+  def display_game_state(game)
+    @output_stream.puts "\e[2J\e[f"
+    @output_stream.puts "The deck has #{game.deck.count} cards in it"
+    @output_stream.puts "the discard has #{game.discardPile.length} card(s) in it"
+    @output_stream.puts "here is the current goal: #{game.goal}"
+    @output_stream.puts "here are the current rules:#{game.ruleBase}"
+  end
+end
+
+class TestInterface < BaseTextInterface
 
   attr_reader :card_list
   attr_reader :prompted
 
   def initialize(input, output)
+    super()
     @input_stream = input
     @output_stream = output
   end
@@ -75,25 +82,6 @@ class TestInterface
   def choose_from_list(card_list, prompt)
     @prompted = true
     @card_list = card_list
-    whichCard = @input_stream.gets.strip.to_i
-    card_list.delete_at(whichCard)
-  end
-
-  def ask_yes_no(prompt)
-    @output_stream.puts "#{prompt} (y/N)"
-    response = get_input
-    response == 'y' || response == 'Y'
-  end
-
-  def ask_rotation(prompt="Which direction?")
-    @output_stream.puts "#{prompt} (clockwise/CounterClockwise)"
-    response = get_input
-    response.start_with?("cl") ? Direction::Clockwise : Direction::CounterClockwise
-  end
-
-  private
-  def get_input
-    input = @input_stream.gets
-    input.strip
+    method(:choose_from_list).super_method.call(card_list, prompt)
   end
 end
