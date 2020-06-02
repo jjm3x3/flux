4dcdf891d951a68fa2e31173463b6daf1e45be8a
Jacob Meixner <jjm3333@gmail.com>
2020-06-01 21:21:53 -0700

    Make the players keepers a readonly property

--- tests/game_spec.rb
+++ tests/game_spec.rb
@@ -129,7 +129,9 @@ describe "game" do
             testInterface = TestInterface.new(input_stream, test_outfile)
             theGame = Game.new(numberOfPlayers=3, testLogger, testInterface)
             theFirstPlayer = theGame.players[0]
-            theFirstPlayer.keepers = [Keeper.new(0, "thing1"), Keeper.new(0, "thing2"), Keeper.new(0, "thing3")]
+            theFirstPlayer.add_permanent(Keeper.new(0, "thing1"))
+            theFirstPlayer.add_permanent(Keeper.new(0, "thing2"))
+            theFirstPlayer.add_permanent(Keeper.new(0, "thing3"))
             keeperLimit = 2
             theGame.ruleBase.addRule(Limit.new("keeper limit 2", 4, "some dumb rules text", keeperLimit))
 
