13bdb751f44bcaccafadaa83829f3fd415a15043
Jacob Meixner <jjm3333@gmail.com>
2020-06-07 17:59:00 -0700

    Call chose_from_list in draw_2_and_use_em_prompt with play_first_prompt

--- game.rb
+++ game.rb
@@ -184,7 +184,7 @@ class Game
   def draw_2_and_use_em(player)
     @logger.debug "happens sync at the beginning of draw_2_and_use_em"
     cardsDrawn = drawCards(player, 2)
-    select_result = @interface.await.choose_from_list(cardsDrawn, "Which one would you like to play first?")
+    select_result = @interface.await.choose_from_list(cardsDrawn, :play_first_prompt)
     @logger.debug "Here is the selected card in draw_2_and_use_em: '#{select_result.value}'"
     selected_card = select_result.value
     selected_card.play(player, self)
