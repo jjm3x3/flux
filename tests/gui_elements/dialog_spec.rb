f3e326cc9e5f406a7547ceccf6468585a5370075
Jaocb Meixner <jjm3333@gmail.com>
2022-05-15 19:14:39 -0700

    Add a test for new dialog functionality

--- tests/gui_elements/dialog_spec.rb
+++ tests/gui_elements/dialog_spec.rb
@@ -32,5 +32,32 @@ describe "CardDialog" do
             # execute
             sut.draw
         end
+
+        it "should not call draw_text on font when prompt symbol is passed" do
+            # setup
+            gui_double = double("gui")
+            background_double = double("background", draw: nil)
+            font_double = instance_double("font")
+            input_stream = StringIO.new("")
+            test_logger = TestLogger.new(input_stream, test_outfile)
+            prompt_image_double = double("prompt image", draw: nil)
+            expected_prompt_key = :some_expected_prompt
+            sut = CardDialog.new(
+                gui_double,
+                background_double,
+                font_double,
+                test_logger,
+                {expected_prompt_key => prompt_image_double})
+
+            # setup lite
+            sut.set_prompt(expected_prompt_key)
+            sut.show
+
+            # set expectations (Assertaions, Test) first
+            expect(font_double).not_to receive(:draw_text).with(any_args)
+
+            # execute
+            sut.draw
+        end
     end
 end
