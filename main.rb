d78b6712c2515fe3cad053c0ba577b20cd1866d7
Jaocb Meixner <jjm3333@gmail.com>
2022-05-28 16:38:40 -0700

    Add missing commas

--- main.rb
+++ main.rb
@@ -24,7 +24,7 @@ puts "starting game where debug: #{debug} and gui: #{gui}"
 
 prompt_strings = {
   default: "Some default prompt",
-  discard_down_to_keeper_limit: "Choose a keeper to discard"
+  discard_down_to_keeper_limit: "Choose a keeper to discard",
   play_first_prompt: "Which one would you like to play first?",
   play_next_prompt: "which would you like to play next?",
   which_player_to_pick_from_prompt: "which player would you like to pick from",
@@ -34,7 +34,7 @@ prompt_strings = {
   holiday_anniversary_prompt: "Is today a holiday or an anniversary",
   replay_prompt: "pick a card you would like to replay",
   trade_hands_prompt: "who would you like to trade hands with?",
-  rotation_prompt: "Which way would you like to rotate?"
+  rotation_prompt: "Which way would you like to rotate?",
   pick_a_keeper_from_prompt: "Which player would you like to take a keeper from",
   are_you_sure_no_trade_prompt: "Are you sure you don't want to trade with anyone?",
   select_a_keeper_prompt: "Slect which Keeper you would like",
