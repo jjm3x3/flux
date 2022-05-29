6a7f8d1c0c3872653a7e31386bab7e2e72c4a754
Jaocb Meixner <jjm3333@gmail.com>
2022-05-28 18:40:14 -0700

    Move static prompts into their own file entirely

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require "./game_interface.rb"
 require "./game_cli.rb"
 require "./game_gui.rb"
 require "./game_driver.rb"
+require "./constants/prompts.rb"
 
 debug=false
 gui=false
@@ -22,33 +23,13 @@ end
 puts "starting game where debug: #{debug} and gui: #{gui}"
 
 
-prompt_strings = {
-  default: "Some default prompt",
-  discard_down_to_keeper_limit: "Choose a keeper to discard",
-  play_first_prompt: "Which one would you like to play first?",
-  play_next_prompt: "which would you like to play next?",
-  which_player_to_pick_from_prompt: "which player would you like to pick from",
-  select_a_card_to_play_prompt: "Select a card from your hand to play",
-  choose_card_to_play_prompt: "pick a card to play",
-  birthday_prompt: "is today your birthday",
-  holiday_anniversary_prompt: "Is today a holiday or an anniversary",
-  replay_prompt: "pick a card you would like to replay",
-  trade_hands_prompt: "who would you like to trade hands with?",
-  rotation_prompt: "Which way would you like to rotate?",
-  pick_a_keeper_from_prompt: "Which player would you like to take a keeper from",
-  are_you_sure_no_trade_prompt: "Are you sure you don't want to trade with anyone?",
-  select_a_keeper_prompt: "Slect which Keeper you would like",
-  keeper_to_give_prompt: "Which player would you like to take a keeper from",
-  death_discard_prompt: "Which permanent would you like to discard to death?"
-}
-
 logger = CliLogger.new(debug)
 if gui
-  guiGame = GameGui.new(logger, prompt_strings)
+  guiGame = GameGui.new(logger, Constants::PROMPT_STRINGS)
   guiGame.show
 else
   players = Player.generate_players(3)
-  cli_interface = CliInterface.new(prompt_strings)
+  cli_interface = CliInterface.new(Constants::PROMPT_STRINGS)
   theGame = Game.new(logger, cli_interface, players)
   theGame.setup
   gameDriver = GameCli.new(theGame, logger, GameDriver.new(theGame, logger), cli_interface)
