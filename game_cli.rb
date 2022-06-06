dd22b1803398e0ed1347f68fae9f5b641f89b92e
Jaocb Meixner <jjm3333@gmail.com>
2022-06-05 19:19:35 -0700

    Add code that fixes the problem and passes the test

--- game_cli.rb
+++ game_cli.rb
@@ -17,7 +17,8 @@ class GameCli
         @new_game_driver.await.setup_new_turn
         hand = activePlayer.hand
         cardsPlayed = 0
-        while !@new_game_driver.await.turn_over?.value
+        is_turn_over = false
+        while !is_turn_over
           @interface.await.print_permanents(activePlayer, prompt="here are the permanents you have:")
 
           cardToPlay = @interface.await.choose_from_list(hand, :select_a_card_to_play_prompt).value
@@ -35,7 +36,7 @@ class GameCli
           @logger.debug "Check if winner"
           break if @new_game_driver.await.has_winner.value
           @logger.debug "no winner coninute"
-          @new_game_driver.await.post_card_play_clean_up
+          is_turn_over = @new_game_driver.await.post_card_play_clean_up.value
 
           if cardsPlayed > 1000 # just some really absurd number to make sure this stops in case of a bug
             raise "Somehow 1000 cards were played this should not be possible"
