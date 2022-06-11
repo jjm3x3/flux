26310b6647c574cb1ac975995cdcbc5a625394e6
Jaocb Meixner <jjm3333@gmail.com>
2022-06-10 20:28:58 -0700

    Remove a bunch of extra stuffs

--- gui_elements/button.rb
+++ gui_elements/button.rb
@@ -7,26 +7,15 @@ class Button
         @z = z
         @font = font
         @visible = true
-        puts "options for button: '#{text}' is: '#{options}'"
-        @is_pressed = nil
-        @pressed_color = nil
-        @unpressed_color = nil
-        if options
-            puts "options being intialized"
-            @is_pressed = options[:is_pressed]
-            @pressed_color = options[:pressed_color]
-            @unpressed_color = options[:unpressed_color]
-        end
-        puts "in ctor pressed_color is: '#{@pressed_color}' unpressed_color is: '#{@unpressed_color}'"
+        @is_pressed = options[:is_pressed]
+        @pressed_color = options[:pressed_color]
+        @unpressed_color = options[:unpressed_color]
     end
 
     def draw
-        puts "drawing #{@text}"
         left_click_down = @is_pressed.call
 
-        puts "pressed_color is: '#{@pressed_color}' unpressed_color is: '#{@unpressed_color}'"
         textcolor = left_click_down && intersects ? @pressed_color : @unpressed_color
-        puts "textcolor is: '#{textcolor}'"
         @font.draw_text(@text, @x , @y, @z , 1.0, 1.0, textcolor)
     end
 
