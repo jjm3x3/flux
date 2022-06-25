199c8995fbbc23e5f562f7aa307e681cf5f9c956
Jaocb Meixner <jjm3333@gmail.com>
2022-06-25 16:48:48 -0700

    Remove commented out code

--- game_gui.rb
+++ game_gui.rb
@@ -33,7 +33,6 @@ class GameGui < Gosu::Window
 
         dialog_prompts = initialize_dialog_prompts(prompt_strings)
 
-        # @are_you_sure_dialog = Dialog.new(self, @button_options)
         @are_you_sure_dialog = SimpleDialog.new(
             self,
             dialog_background,
@@ -110,11 +109,6 @@ class GameGui < Gosu::Window
                     @logger.debug "GameGui:button_up: are you sure dialog result is: #{current_result}"
                     if current_result == "Yes"
                         start_a_new_game
-                    # should do things for other cases
-                    # elsif clicked == :no_clicked
-                    #     puts "no selected"
-                    # else
-                    #     puts "nothing selected"
                     end
                 end
                 return
