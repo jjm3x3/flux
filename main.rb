df19bdbdaae964622d8f6cc7204159e4f1296a64
Jaocb Meixner <jjm3333@gmail.com>
2022-06-25 12:04:38 -0700

    Use debug option prased from option parse

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 #!/usr/local/bin/ruby
+require "optparse"
 require "./game.rb"
 require "./game_interface.rb"
 require "./game_cli.rb"
@@ -6,6 +7,15 @@ require "./game_gui.rb"
 require "./game_driver.rb"
 require "./constants/prompts.rb"
 require "./constants/stacked_decks.rb"
+
+options = {}
+OptionParser.new do |opt|
+  opt.on("--debug") { |o| options[:debug] = o }
+end.parse!
+
+puts options
+
+# exit 0
 
 debug=false
 gui=false
@@ -21,10 +31,10 @@ ARGV.each do |arg|
   end
 end
 
-puts "starting game where debug: #{debug} and gui: #{gui}"
+puts "starting game where debug: #{options[:debug]} and gui: #{gui}"
 
 logger = Logger.new($stdout)
-logger.level = debug ? Logger::DEBUG : Logger::INFO
+logger.level = options[:debug] ? Logger::DEBUG : Logger::INFO
 
 the_deck = Deck.new(logger)
 # the_deck = StackedDecks.stacked_deck_factory(logger, StackedDecks::QUICK_WIN)
