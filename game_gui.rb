245d15232ecd0a2052e5708f30cba047de95dbc6
jjm3x3 <jjm3x3@users.noreply.github.com>
2022-07-03 18:07:53 -0700

    Feature/refactor draw (#65)
    * Move simple_dialog draw for menu into main menu branch
    
    * Move current_dialog draw to end of draw since it should always be on top
    
    * Move visibility toggling of new_game_button
    
    * Remove else case since this is how the rest of the game is drawn
    
    * Also draw are_you_sure_dialog

--- game_gui.rb
+++ game_gui.rb
@@ -106,7 +106,9 @@ class GameGui < Gosu::Window
             if @simple_dialog && @simple_dialog.is_visible?
                 @simple_dialog.handle_result do |result|
                     @logger.debug "GameGui:button_up: are you sure dialog result is: #{result}"
+                    @simple_dialog.hide
                     if result == "Yes"
+                        @new_game_button.set_visibility false
                         start_a_new_game
                     end
                     # TODO:: do things for other cases
@@ -174,54 +176,51 @@ class GameGui < Gosu::Window
     def draw
         @bakground_image.draw(0,0,0)
 
-        @simple_dialog.draw
-        if @list_dialog != nil
-            @list_dialog.draw
-        end
         if !@new_game_driver
-        # for main menu
+            # for main menu
             @new_game_button.draw
-        else
-            @new_game_button.set_visibility(false)
-            @simple_dialog.hide
-            @game_stats.draw(@game)
+            @simple_dialog.draw
+            return
+        end
+        @game_stats.draw(@game)
 
-            activePlayer = @current_cached_player
-            @font.draw_text("It is player #{activePlayer}'s turn'", 10, 10 + @game_stats.height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
+        activePlayer = @current_cached_player
+        @font.draw_text("It is player #{activePlayer}'s turn'", 10, 10 + @game_stats.height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
 
-            @font.draw_text("Here are the permanents they have:", 10, 10 + @game_stats.height + 10 + @font.height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
+        @font.draw_text("Here are the permanents they have:", 10, 10 + @game_stats.height + 10 + @font.height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
 
-            permanentsDisplayed = 0
-            permananent_margin = 5
-            permanents_start_y = 10 + @game_stats.height + 10 + @font.height + 10 + @font.height + permananent_margin
-            activePlayer.permanents.each do |card|
-                next_y = permanents_start_y + @font.height * permanentsDisplayed + permananent_margin * permanentsDisplayed
-                @font.draw_text("#{card}", 20, next_y, 1, 1.0, 1.0, Gosu::Color::WHITE)
-                permanentsDisplayed += 1
+        permanentsDisplayed = 0
+        permananent_margin = 5
+        permanents_start_y = 10 + @game_stats.height + 10 + @font.height + 10 + @font.height + permananent_margin
+        activePlayer.permanents.each do |card|
+            next_y = permanents_start_y + @font.height * permanentsDisplayed + permananent_margin * permanentsDisplayed
+            @font.draw_text("#{card}", 20, next_y, 1, 1.0, 1.0, Gosu::Color::WHITE)
+            permanentsDisplayed += 1
+        end
+        permanents_height = activePlayer.permanents.length * @font.height + activePlayer.permanents.length * permananent_margin
+
+        @font.draw_text("Pick a card to play:", 10, permanents_start_y + permanents_height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
+
+        if @redraw_hand
+
+            cardsDisplayed = 0
+            @current_displayed_cards = []
+            activePlayer.hand.each do |card|
+                newCardButton = Button.new(self, @font, "#{card}", 20, (permanents_start_y + permanents_height + 10 + @font.height) + 10 * cardsDisplayed + @font.height * cardsDisplayed, ZOrder::GAME_ITEMS, @button_options)
+                newCardButton.draw
+                @current_displayed_cards << newCardButton
+                cardsDisplayed += 1
             end
-            permanents_height = activePlayer.permanents.length * @font.height + activePlayer.permanents.length * permananent_margin
 
-            @font.draw_text("Pick a card to play:", 10, permanents_start_y + permanents_height + 10, 1, 1.0, 1.0, Gosu::Color::WHITE)
+            @redraw_hand = false
 
-            if @redraw_hand
-
-                cardsDisplayed = 0
-                @current_displayed_cards = []
-                activePlayer.hand.each do |card|
-                    newCardButton = Button.new(self, @font, "#{card}", 20, (permanents_start_y + permanents_height + 10 + @font.height) + 10 * cardsDisplayed + @font.height * cardsDisplayed, ZOrder::GAME_ITEMS, @button_options)
-                    newCardButton.draw
-                    @current_displayed_cards << newCardButton
-                    cardsDisplayed += 1
-                end
-
-                @redraw_hand = false
-
-            else
-                @current_displayed_cards.each do |cardButton|
-                    cardButton.draw
-                end
+        else
+            @current_displayed_cards.each do |cardButton|
+                cardButton.draw
             end
         end
+        @simple_dialog.draw
+        @list_dialog.draw
     end
 
     def get_dialog_result
