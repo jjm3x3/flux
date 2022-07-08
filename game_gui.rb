48dcec81c3e88e9fd613794d7fa657622da0ca6b
Jaocb Meixner <jjm3333@gmail.com>
2022-07-07 23:05:08 -0700

    Enable moving the simple_dialog

--- game_gui.rb
+++ game_gui.rb
@@ -155,11 +155,16 @@ class GameGui < Gosu::Window
         if Gosu.button_down? Gosu::MS_LEFT and !@left_click_down
             @logger.debug "left button click"
             @left_click_down = true
-            @dialog_clicked = @list_dialog.check_clicked_at(mouse_x, mouse_y)
+            @list_dialog_clicked = @list_dialog.check_clicked_at(mouse_x, mouse_y)
+            @simple_dialog_clicked = @simple_dialog.check_clicked_at(mouse_x, mouse_y)
         end
 
-        if @left_click_down && @dialog_clicked
-            @list_dialog.set_relative_position(mouse_x, mouse_y)
+        if @left_click_down 
+            if @list_dialog_clicked
+                @list_dialog.set_relative_position(mouse_x, mouse_y)
+            elsif @simple_dialog_clicked
+                @simple_dialog.set_relative_position(mouse_x, mouse_y)
+            end
         end
 
         if @new_game_driver
