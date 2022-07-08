44031c97a1a6540f27ec4cd0ed6ab703290e5d07
Jaocb Meixner <jjm3333@gmail.com>
2022-07-07 23:30:03 -0700

    Extract outputName var in build script

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,3 +1,4 @@
 #!/usr/bin/env pwsh
 rbenv shell 2.
-ocra .\main.rb cards.db --gemfile .\Gemfile --chdir-first --windows --dll ruby_builtin_dlls\libssp-0.dll --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\libgcc_s_seh-1.dll --dll ruby_builtin_dlls\libwinpthread-1.dll --dll libsqlite3-0.dll --output fluxx_v0.1.0.exe
+$OutputName = "fluxx_v0.1.0.exe"
+ocra .\main.rb cards.db --gemfile .\Gemfile --chdir-first --windows --dll ruby_builtin_dlls\libssp-0.dll --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\libgcc_s_seh-1.dll --dll ruby_builtin_dlls\libwinpthread-1.dll --dll libsqlite3-0.dll --output $OutputName
