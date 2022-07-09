104366a964ec59d83c9ff36748a4e1de9297c875
Jaocb Meixner <jjm3333@gmail.com>
2022-07-09 13:49:32 -0700

    Add a test for the button::set_position

--- tests/gui_elements/button_spec.rb
+++ tests/gui_elements/button_spec.rb
@@ -29,5 +29,23 @@ describe "Button" do
             # nothing much to assert just making sure this works
         end
     end
+
+    describe "set_position" do
+        it "should draw based what what is porvided in set_position" do
+            # setup
+            window_double = double("window")
+            font_double = double("font", draw_text: nil)
+            sut = Button.new(window_double, font_double, "HI", 1,1,1, {is_pressed: ->() {} })
+            expected_x = 100
+            expected_y = 100
+
+            # execute
+            sut.set_position(expected_x, expected_y)
+            sut.draw
+
+            # assert
+            expect(font_double).to have_received(:draw_text).with(anything, expected_x, expected_y, anything, anything, anything, anything)
+        end
+    end
 end
 
