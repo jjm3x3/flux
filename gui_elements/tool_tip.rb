3ccf63225d404c0ada97463114ad7fab718aca4a
Jaocb Meixner <jjm3333@gmail.com>
2022-07-08 18:44:13 -0700

    Shift over text_image

--- gui_elements/tool_tip.rb
+++ gui_elements/tool_tip.rb
@@ -14,7 +14,7 @@ class ToolTip
                 if diff > 1
                     my_green = Gosu::Color.new(255,0, 128, 0)
                     Gosu::draw_rect(@window.mouse_x, @window.mouse_y - (@image.height + 6), @image.width + 6, @image.height + 6, my_green, ZOrder::DIALOG)
-                    @image.draw(@window.mouse_x + 3, (@window.mouse_y - @image.height) - 3, ZOrder::DIALOG_ITEMS)
+                    @image.draw(@window.mouse_x + 6, (@window.mouse_y - @image.height) - 3, ZOrder::DIALOG_ITEMS)
                 end
             end
         else
