b38e66d881727b4d7ad3b8e72a7db18dd0153078
Jaocb Meixner <jjm3333@gmail.com>
2022-07-08 18:44:30 -0700

    Change background color of tool_tip

--- gui_elements/tool_tip.rb
+++ gui_elements/tool_tip.rb
@@ -12,7 +12,7 @@ class ToolTip
             else
                 diff = Time.now - @timer_start
                 if diff > 1
-                    my_green = Gosu::Color.new(255,0, 128, 0)
+                    my_green = Gosu::Color.new(255, 53, 112, 53)
                     Gosu::draw_rect(@window.mouse_x, @window.mouse_y - (@image.height + 6), @image.width + 6, @image.height + 6, my_green, ZOrder::DIALOG)
                     @image.draw(@window.mouse_x + 6, (@window.mouse_y - @image.height) - 3, ZOrder::DIALOG_ITEMS)
                 end
