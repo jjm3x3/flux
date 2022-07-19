2454205848612cb39031177f141f13faf7d062a3
Jaocb Meixner <jjm3333@gmail.com>
2022-07-18 22:41:25 -0700

    Draw permanents to expand horizontally

--- gui_elements/player_permanents.rb
+++ gui_elements/player_permanents.rb
@@ -9,9 +9,14 @@ class PlayerPermanents
         permanentsDisplayed = 0
         permananent_margin = 5
         permanents_start_y = y + 30
+        perm_x = x + 20
         active_player.permanents.each do |card|
+            if permanentsDisplayed >= 3
+                permanentsDisplayed = 0
+                perm_x += 100
+            end
             next_y = permanents_start_y + @font.height * permanentsDisplayed + permananent_margin * permanentsDisplayed
-            @font.draw_text("#{card}", x, next_y, 1, 1.0, 1.0, Gosu::Color::WHITE)
+            @font.draw_text("#{card}", perm_x, next_y, 1, 1.0, 1.0, Gosu::Color::WHITE)
             permanentsDisplayed += 1
         end
         # permanents_height = activePlayer.permanents.length * @font.height + activePlayer.permanents.length * permananent_margin
