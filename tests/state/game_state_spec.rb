7449566802606cf97ea9945aee84b8b9180e33b5
Jaocb Meixner <jjm3333@gmail.com>
2022-08-16 19:50:00 -0700

    Add a first test for game_state

---
+++ tests/state/game_state_spec.rb
@@ -0,0 +1,17 @@
+require "./state/game_state.rb"
+
+describe "game_state" do
+    describe "initialize" do
+        it "should construct" do
+            # setup
+            #   nothign to do
+
+            # execute
+            sut = GameState.new(10)
+
+            # test 
+            #   should not throw ex
+        end
+
+    end
+end
