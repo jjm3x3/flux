1836ddf56576cf615838823c42ab2973e1d6c19d
Jaocb Meixner <jjm3333@gmail.com>
2022-08-18 20:52:06 -0700

    Remove all the delay code and just draw outright

--- gui_elements/tool_tip.rb
+++ gui_elements/tool_tip.rb
@@ -5,23 +5,9 @@ class ToolTip
     end
 
     def draw
-
-        if @prevous_x == @window.mouse_x && @prevous_y == @window.mouse_y 
-            if !@timer_start
-                @timer_start = Time.now
-            else
-                diff = Time.now - @timer_start
-                if diff > 1
-                    my_green = Gosu::Color.new(255, 53, 112, 53)
-                    Gosu::draw_rect(@window.mouse_x, @window.mouse_y - (@image.height + 6), @image.width + 6, @image.height + 6, my_green, ZOrder::DIALOG)
-                    @image.draw(@window.mouse_x + 6, (@window.mouse_y - @image.height) - 3, ZOrder::DIALOG_ITEMS)
-                end
-            end
-        else
-            @timer_start = nil
-        end
-        @prevous_x = @window.mouse_x
-        @prevous_y = @window.mouse_y
+        my_green = Gosu::Color.new(255, 53, 112, 53)
+        Gosu::draw_rect(@window.mouse_x, @window.mouse_y - (@image.height + 6), @image.width + 6, @image.height + 6, my_green, ZOrder::DIALOG)
+        @image.draw(@window.mouse_x + 6, (@window.mouse_y - @image.height) - 3, ZOrder::DIALOG_ITEMS)
     end
 
 end
