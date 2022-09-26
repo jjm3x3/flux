8397887acf1d19dfc967bcc45cdcc36e84039ffc
Jaocb Meixner <jjm3333@gmail.com>
2022-09-25 21:41:30 -0700

    Update version number in windows build script

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 rbenv shell 2.
-$OutputName = "fluxx_v0.1.0.exe"
+$OutputName = "fluxx_v0.2.0.exe"
 ocra .\main.rb cards.db --gemfile .\Gemfile --chdir-first --windows --dll ruby_builtin_dlls\libssp-0.dll --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\libgcc_s_seh-1.dll --dll ruby_builtin_dlls\libwinpthread-1.dll --dll libsqlite3-0.dll --output $OutputName
